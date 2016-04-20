fa6e6ff8098d293778c571fb17477db305b84acb
Joshua Shin <joshshin8@gmail.com>
2016-04-20 14:53:24 -0700

    troubleshooting iframe/window scope

--- client/templates/exit/exit.js
+++ client/templates/exit/exit.js
@@ -1,5 +1,6 @@
 Template.exit.helpers({
 	assignmentId: function(){
+		console.log('document',document)
 		var assignmentId = $("input[name='hitId']").getAttribute('value');
 		console.log('assignmentId', assignmentId);
 		return assignmentId
