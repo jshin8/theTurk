d7b7c84e493731683aa37e33bea42a72625225d0
Joshua Shin <joshshin8@gmail.com>
2016-04-20 15:17:54 -0700

    troubleshooting more

--- client/templates/exit/exit.js
+++ client/templates/exit/exit.js
@@ -1,9 +1,28 @@
 Template.exit.helpers({
 	assignmentId: function(){
-		var doc = window.frameElement
-		console.log('document',doc)
+		var parentWindow = window.parent; 
+		var href = location.href;
+		console.log('parentWindow', parentWindow);
+		console.log('href',href)
+
+		var gup = gup('hitId');
+		console.log('gup',gup)
+
 		var assignmentId = $("input[name='hitId']").getAttribute('value');
 		console.log('assignmentId', assignmentId);
 		return assignmentId
 	}
 })
+
+
+function gup( name )
+{
+  name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+  var regexS = "[\\?&]"+name+"=([^&#]*)";
+  var regex = new RegExp( regexS );
+  var results = regex.exec( window.location.href );
+  if( results == null )
+    return "";
+  else
+    return results[1];
+}
